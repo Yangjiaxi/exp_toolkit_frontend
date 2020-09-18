581f0f7ec4d889373239c99bcc6437f851ca0b6d
Muxv <85579998@qq.com>
2020-09-18 14:51:59 +0800

    <ADD> about page

--- src/pages/About/component.jsx
+++ src/pages/About/component.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useEffect } from "react";
 
-import { Typography, Paper } from "@material-ui/core";
+import { Typography, Paper, List, ListItem } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/styles";
 
 import { PAGE_NAME_DICT } from "../consts";
@@ -16,6 +16,12 @@ const useStyles = makeStyles(({ spacing }) =>
     key: {
       marginTop: spacing(2),
     },
+    content: {
+      fontSize: "0.7em",
+    },
+    title: {
+      fontSize: "1em",
+    },
   }),
 );
 
@@ -27,7 +33,32 @@ const About = memo(({ changeBrowserPath }) => {
   return (
     <Paper className={classes.root}>
       <Typography variant="h4" align="center">
-        这是关于
+        <List>
+          <ListItem className={classes.title}>作者：</ListItem>
+          <ListItem>
+            <Typography className={classes.content}>杨家玺 沈嘉杰</Typography>
+          </ListItem>
+          <ListItem className={classes.title}>分工：</ListItem>
+          <ListItem>
+            <Typography className={classes.content}>
+              杨家玺：后端, Python接口
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography className={classes.content}>沈嘉杰：前端</Typography>
+          </ListItem>
+          <ListItem className={classes.title}>技术栈</ListItem>
+          <ListItem>
+            <Typography className={classes.content}>
+              后端: Express.js MongoDB
+            </Typography>
+          </ListItem>
+          <ListItem>
+            <Typography className={classes.content}>
+              前端: React.js Redux Rxjs Material-UI
+            </Typography>
+          </ListItem>
+        </List>
       </Typography>
     </Paper>
   );
